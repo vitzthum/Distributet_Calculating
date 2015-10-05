6b07d9b82e654ebdd48a33db6b6a129bc0e3ab6e
Michael <vitzthum_m@gmx.at>
2015-10-05 22:55:57 +0200

    do it x Times without working average

--- src/hello.go
+++ src/hello.go
@@ -1,13 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+const Times = 5
 
 func main() {
-	fmt.Println("\ncounting")
 
-	for i := 0; i < 10; i++ {
-		fmt.Println(i)
+	var durationPerRound [Times]time.Duration
+//	var averageTime int
+
+	for i := 0; i < Times; i++ {
+
+		fmt.Println("\n")
+		fmt.Println("----------------------")
+		fmt.Printf("\ncalculating: %v\n\n", i+1)
+
+		x := 0
+
+		bevore := time.Now()
+		for i := 1; i < 100000; i++ {
+			for j := 1; j < 8000; j++ {
+				y := i+j
+				if y >= 0 {
+					x += 1
+				}
+			}
+		}
+		
+		duration := time.Since(bevore)
+		
+		fmt.Printf("about %v Mio. calculations done in:  %v\n", x/1e6, duration)
+		fmt.Println("\n----------------------\n")
+
+		durationPerRound[i] = duration
+
+		fmt.Println(duration.Hours)
 	}
 
-	fmt.Println("done\n")
+	for i := 0; i < Times; i++ {
+//		averageTime += time.Nanoseconds(durationPerRound[i])
+	}
+//	averageTime /= Times
+
 }
