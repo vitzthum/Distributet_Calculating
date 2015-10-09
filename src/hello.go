7ddba031f4e1f311cb52576c6db9259ec96bc36e
Michael <vitzthum_m@gmx.at>
2015-10-09 10:05:03 +0200

    distribute stuff to x Threads

--- src/hello.go
+++ src/hello.go
@@ -11,16 +11,56 @@ const Threads = 10
 var wg sync.WaitGroup
 const Calculations = 1e10
 
-func calculate() () {
+type Vertex struct {
+	start, end int
+}
+
+
+func guess(x Vertex) {
 	defer wg.Done()
 
-	for i := 1; i <= (Calculations/1e3)/Threads; i++ {
-		for j := 1; j <= 1e3; j++ {
-			var _ = i+j
-		}
+	for i := x.start; i <= x.end; i++ {
 	}
+
 	return
 }
+
+func main() {
+	runtime.GOMAXPROCS(Threads)
+	wg.Add(Threads)
+
+	fmt.Println("")
+	fmt.Print("type a value (0 - 1000): ")
+	fmt.Scanln()
+
+	var ranges [Threads]Vertex
+	for i := 0; i < Threads; i++ {
+		ranges[i].start = Calculations/Threads * i
+		ranges[i].end = Calculations/Threads * (i+1) - 1
+	}
+
+	before := time.Now()
+
+	for i := 0; i < Threads; i++ {
+		go guess(ranges[i])
+	}
+
+	wg.Wait()
+	duration := time.Since(before)
+
+	fmt.Println("\n----------------------\n")
+	fmt.Printf("Threads     : %v\n", Threads)
+	fmt.Printf("Calculations: %v Billions\n", Calculations/1e9)
+	fmt.Printf("done in     : %v\n", duration)
+	fmt.Println("\n----------------------\n")
+
+	// calculate average duration
+//	averageDuration := calculateAverage(durations)
+
+	// show average duration
+//	showAverage(averageDuration, calculations)
+}
+
 /*
 func calculateAverage(durations [Threads]time.Duration) (averageDuration int) {
 	for i := 0; i < Threads; i++ {
@@ -43,28 +83,3 @@ func showAverage(averageDuration int, calculations int) {
 	fmt.Println("\n----------------------\n")
 }
 */
-func main() {
-	runtime.GOMAXPROCS(Threads)
-	wg.Add(Threads)
-
-	before := time.Now()
-
-	for i := 0; i < Threads; i++ {
-		go calculate()
-	}
-
-	wg.Wait()
-	duration := time.Since(before)
-
-	fmt.Println("\n----------------------\n")
-	fmt.Printf("Threads     : %v\n", Threads)
-	fmt.Printf("Calculations: %v Mio.\n", Calculations/1e6)
-	fmt.Printf("done in     : %v\n", duration)
-	fmt.Println("\n----------------------\n")
-
-	// calculate average duration
-//	averageDuration := calculateAverage(durations)
-
-	// show average duration
-//	showAverage(averageDuration, calculations)
-}
